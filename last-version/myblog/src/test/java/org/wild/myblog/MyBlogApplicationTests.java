10b7a8eeb74361560489f604091da56d5b4594c5
odowin <102875944+odowin@users.noreply.github.com>
2025-01-15 15:02:45 +0100

    last-version commit

---
+++ last-version/myblog/src/test/java/org/wild/myblog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wild.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
