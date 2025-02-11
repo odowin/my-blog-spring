3491a4e00f0f0618edfb001d57b965e7d0b6907f
odowin <102875944+odowin@users.noreply.github.com>
2025-02-11 09:55:53 +0100

    spring07

---
+++ spring07/myblog/src/main/java/org/wild/myblog/controller/ArticleController.java
@@ -0,0 +1,222 @@
+package org.wild.myblog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.wild.myblog.dto.ArticleDTO;
+import org.wild.myblog.model.Article;
+import org.wild.myblog.model.Category;
+import org.wild.myblog.model.Image;
+import org.wild.myblog.repository.ArticleRepository;
+import org.wild.myblog.repository.CategoryRepository;
+import org.wild.myblog.repository.ImageRepository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/articles")
+public class ArticleController {
+
+    private final ArticleRepository articleRepository;
+    private final CategoryRepository categoryRepository;
+    private final ImageRepository imageRepository;
+
+    public ArticleController(
+            ArticleRepository articleRepository,
+            CategoryRepository categoryRepository,
+            ImageRepository imageRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.categoryRepository = categoryRepository;
+        this.imageRepository = imageRepository;
+    }
+
+    private ArticleDTO convertToDTO(Article article) {
+        ArticleDTO articleDTO = new ArticleDTO();
+        articleDTO.setId(article.getId());
+        articleDTO.setTitle(article.getTitle());
+        articleDTO.setContent(article.getContent());
+        articleDTO.setUpdatedAt(article.getUpdatedAt());
+        if (article.getCategory() != null) {
+            articleDTO.setCategoryName(article.getCategory().getName());
+        }
+        if (article.getImages() != null) {
+            articleDTO.setImageUrls(article.getImages().stream().map(Image::getUrl).collect(Collectors.toList()));
+        }
+        return articleDTO;
+    }
+
+    @GetMapping
+    //public ResponseEntity<List<Article>> getAllArticles() {
+    public ResponseEntity<List<ArticleDTO>> getAllArticles() {
+        List<Article> articles = articleRepository.findAll();
+        if (articles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        //return ResponseEntity.ok(articles);
+        List<ArticleDTO> articleDTOs = articles.stream().map(this::convertToDTO).collect(Collectors.toList());
+        return ResponseEntity.ok(articleDTOs);
+    }
+
+    @GetMapping("/{id}")
+    // public ResponseEntity<Article> getArticleById(@PathVariable Long id) {
+    public ResponseEntity<ArticleDTO> getArticleById(@PathVariable Long id) {
+
+        Article article = articleRepository.findById(id).orElse(null);
+        if (article == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // return ResponseEntity.ok(article);
+        return ResponseEntity.ok(convertToDTO(article));
+    }
+
+    @GetMapping("/search-title")
+    public ResponseEntity<List<Article>> getArticlesByTitle(@RequestParam String searchTerms) {
+        List<Article> articles = articleRepository.findByTitle(searchTerms);
+        if (articles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(articles);
+    }
+
+    // Endpoint pour chercher des articles contenant un mot-clé dans le contenu
+    @GetMapping("/search-content")
+    public ResponseEntity<List<Article>> getArticlesByContent(@RequestParam String keyword) {
+        List<Article> articles = articleRepository.findByContentContaining(keyword);
+        if (articles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(articles);
+    }
+
+    // Endpoint pour chercher des articles créés après une date donnée
+    @GetMapping("/search-after-date")
+    public ResponseEntity<List<Article>> getArticlesCreatedAfter(@RequestParam String date) {
+        LocalDateTime dateTime = LocalDateTime.parse(date); // Format: "YYYY-MM-DDTHH:MM:SS"
+        List<Article> articles = articleRepository.findByCreatedAtAfter(dateTime);
+        if (articles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(articles);
+    }
+
+    // Endpoint pour récupérer les 5 derniers articles créés
+    @GetMapping("/latest")
+    public ResponseEntity<List<Article>> getLastFiveArticles() {
+        List<Article> articles = articleRepository.findTop5ByOrderByCreatedAtDesc();
+        if (articles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(articles);
+    }
+
+    @PostMapping
+    // public ResponseEntity<Article> createArticle(@RequestBody Article article) {
+    public ResponseEntity<ArticleDTO> createArticle(@RequestBody Article article) {
+        article.setCreatedAt(LocalDateTime.now());
+        article.setUpdatedAt(LocalDateTime.now());
+
+        // Ajout de la catégorie
+        if (article.getCategory() != null) {
+            Category category = categoryRepository.findById(article.getCategory().getId()).orElse(null);
+            if (category == null) {
+                return ResponseEntity.badRequest().body(null);
+            }
+            article.setCategory(category);
+        }
+
+        if (article.getImages() != null && !article.getImages().isEmpty()) {
+            List<Image> validImages = new ArrayList<>();
+            for (Image image : article.getImages()) {
+                if (image.getId() != null) {
+                    // Vérification des images existantes
+                    Image existingImage = imageRepository.findById(image.getId()).orElse(null);
+                    if (existingImage != null) {
+                        validImages.add(existingImage);
+                    } else {
+                        return ResponseEntity.badRequest().body(null);
+                    }
+                } else {
+                    // Création de nouvelles images
+                    Image savedImage = imageRepository.save(image);
+                    validImages.add(savedImage);
+                }
+            }
+            article.setImages(validImages);
+        }
+
+        Article savedArticle = articleRepository.save(article);
+        // return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+        return ResponseEntity.status(HttpStatus.CREATED).body(convertToDTO(savedArticle));
+    }
+
+    @PutMapping("/{id}")
+    // public ResponseEntity<Article> updateArticle(@PathVariable Long id, @RequestBody Article articleDetails) {
+    public ResponseEntity<ArticleDTO> updateArticle(@PathVariable Long id, @RequestBody Article articleDetails) {
+
+        Article article = articleRepository.findById(id).orElse(null);
+        if (article == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        article.setTitle(articleDetails.getTitle());
+        article.setContent(articleDetails.getContent());
+        article.setUpdatedAt(LocalDateTime.now());
+
+        // Mise à jour de la catégorie
+        if (articleDetails.getCategory() != null) {
+            Category category = categoryRepository.findById(articleDetails.getCategory().getId()).orElse(null);
+            if (category == null) {
+                return ResponseEntity.badRequest().body(null);
+            }
+            article.setCategory(category);
+        }
+
+        if (articleDetails.getImages() != null) {
+            List<Image> validImages = new ArrayList<>();
+            for (Image image : articleDetails.getImages()) {
+                if (image.getId() != null) {
+                    // Vérification des images existantes
+                    Image existingImage = imageRepository.findById(image.getId()).orElse(null);
+                    if (existingImage != null) {
+                        validImages.add(existingImage);
+                    } else {
+                        return ResponseEntity.badRequest().build(); // Image non trouvée, retour d'une erreur
+                    }
+                } else {
+                    // Création de nouvelles images
+                    Image savedImage = imageRepository.save(image);
+                    validImages.add(savedImage);
+                }
+            }
+            // Mettre à jour la liste des images associées
+            article.setImages(validImages);
+        } else {
+            // Si aucune image n'est fournie, on nettoie la liste des images associées
+            article.getImages().clear();
+        }
+
+        Article updatedArticle = articleRepository.save(article);
+        // return ResponseEntity.ok(updatedArticle);
+        return ResponseEntity.ok(convertToDTO(updatedArticle));
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
+
+        Article article = articleRepository.findById(id).orElse(null);
+        if (article == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        articleRepository.delete(article);
+        return ResponseEntity.noContent().build();
+    }
+}
